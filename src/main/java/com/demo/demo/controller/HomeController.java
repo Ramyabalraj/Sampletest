c124fc79ce77f7683590a6ba281bff4a230124df
Ramyabalraj <57798910+Ramyabalraj@users.noreply.github.com>
2021-03-22 12:42:13 +0530

    Add files via upload

--- src/main/java/com/demo/demo/controller/HomeController.java
+++ src/main/java/com/demo/demo/controller/HomeController.java
@@ -20,23 +20,10 @@ public class HomeController {
 
 	@RequestMapping(value = "/hello", method = RequestMethod.GET)
     @IgnoreJwt
-	@ResponseBody
+	
 	    public String helloWorld() {
-		  HttpResponse<JsonNode> response = null;
-		try {
-			response = Unirest.get("https://jirauat.atlassian.net/rest/api/3/myself")
-					  .basicAuth("ramya.balraj@kgisl.com", "cUapwmIGGklbxxCVm8NA0C28")
-					  .header("Accept", "application/json")
-					  .asJson();
-		} catch (UnirestException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		   DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");  
-		   LocalDateTime now = LocalDateTime.now();  
-		   System.out.println(dtf.format(now));  
-				System.out.println("********************************"+response.getBody()+"********************************************");
-	        return "Hello World!!!"+"  "+dtf.format(now);
+		 
+	        return "hello";
 	        
 	    }
 }
